a25f5bbf269e1a72896484600ef25d760c2bd9cc
Thomas Chun <tchun2055@gmail.com>
2019-05-11 19:06:03 -0700

    countdown functionality finished

--- src/time-input.js
+++ src/time-input.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import TimeDisplay from './time-display';
 
 export default class TimeInput extends React.Component {
   constructor(props) {
@@ -8,7 +7,9 @@ export default class TimeInput extends React.Component {
       daysValue: 0,
       hoursValue: 0,
       minutesValue: 0,
-      selectedTime: [],
+      secondsValue: 0,
+      totalSeconds: 0,
+      running: false,
     }
   }
 
@@ -16,6 +17,7 @@ export default class TimeInput extends React.Component {
     console.log(event.target.value, 'days');
     this.setState({
       daysValue: event.target.value,
+      totalSeconds: ((event.target.value * 86400) + (this.state.hoursValue * 3600) + (this.state.minutesValue * 60) + (this.state.secondsValue * 1))
     })
   }
 
@@ -23,6 +25,7 @@ export default class TimeInput extends React.Component {
     console.log(event.target.value, 'hours');
     this.setState({
       hoursValue: event.target.value,
+      totalSeconds: ((this.state.daysValue * 86400) + (event.target.value * 3600) + (this.state.minutesValue * 60) + (this.state.secondsValue * 1))
     })
   }
 
@@ -30,36 +33,99 @@ export default class TimeInput extends React.Component {
     console.log(event.target.value, 'minutes');
     this.setState({
       minutesValue: event.target.value,
+      totalSeconds: ((this.state.daysValue * 86400) + (this.state.hoursValue * 3600) + (event.target.value * 60) + (this.state.secondsValue * 1))
     })
   }
 
-  handleSubmit(event, days, hours, minutes) {
-    event.preventDefault();
-    let daysText = '', hoursText = '', minText = '';
-    days === '1' ? daysText = 'day' : daysText = 'days';
-    hours === '1' ? hoursText = 'hour' : hoursText = 'hours';
-    minutes === '1' ? minText = 'minute' : minText = 'minutes';
-    console.log(`${days} ${daysText} ${hours} ${hoursText} ${minutes} ${minText}`);
+  updateSecondsValue(event) {
     this.setState({
-      selectedTime: parseInt((days * 86400) + (hours * 3600) + (minutes * 60)),
+      secondsValue: event.target.value,
+      totalSeconds: ((this.state.daysValue * 86400) + (this.state.hoursValue * 3600) + (this.state.minutesValue * 60) + (event.target.value * 1))
     })
   }
 
-  handleStart() {
-    console.log('start clicked');
+  countdown() {
+    if (this.state.totalSeconds > 0) {
+      this.setState({
+        totalSeconds: parseInt(this.state.totalSeconds - 1),
+      })
+    } else {
+      clearInterval(this.timerID);
+      this.setState({
+        running: false,
+      })
+    }
+    console.log(this.state.totalSeconds);
+  }
+
+  handleStart(running) {
+    this.setState({
+      running: !this.state.running,
+    })
+    if (this.state.running === true) {
+      clearInterval(this.timerID);
+    } else {
+      this.timerID = setInterval(() => this.countdown(this.state.totalSeconds), 1000);
+    }
+  }
+
+  handleReset() {
+    clearInterval(this.timerID);
+    document.getElementById('days').value = '';
+    document.getElementById('hours').value = '';
+    document.getElementById('minutes').value = '';
+    document.getElementById('seconds').value = '';
+    this.setState({
+      daysValue: 0,
+      hoursValue: 0,
+      minutesValue: 0,
+      secondsValue: 0,
+      totalSeconds: 0,
+    })
   }
 
   render() {
+    let totalSeconds = this.state.totalSeconds;
+    let startBtn = '';
+    this.state.running === true ? startBtn = 'Pause' : startBtn = 'Start'
+    let days = Math.floor(totalSeconds / 86400);
+    let hours = Math.floor((totalSeconds - (days * 86400)) / 3600);
+    let minutes = Math.floor((totalSeconds - (days * 86400) - (hours * 3600)) / 60);
+    let seconds = Math.floor((totalSeconds - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+    let timeDisplay = '', daysText = '', hoursText = '', minText = '', secText='';
+    days === 1 ? daysText = 'day' : daysText = 'days';
+    hours === 1 ? hoursText = 'hour' : hoursText = 'hours';
+    minutes === 1 ? minText = 'minute' : minText = 'minutes';
+    seconds === 1 ? secText = 'second' : secText = 'seconds';
+
+    if (totalSeconds === 0) {
+      timeDisplay = 'Countdown Expired';
+    } else if (days === 0 && hours === 0 && minutes === 0) {
+      timeDisplay = `${seconds}${secText}`;
+    } else if (days === 0 && hours === 0) {
+      timeDisplay = `${minutes} ${minText} ${seconds} ${secText}`;
+    } else if (days === 0) {
+      timeDisplay = `${hours} ${hoursText} ${minutes} ${minText} ${seconds} ${secText}`;
+    } else {
+      timeDisplay = `${days} ${daysText} ${hours} ${hoursText} ${minutes} ${minText} ${seconds} ${secText}`;
+    }
+
     return (
       <div>
-        <input className='Days' type='number' min='0'  onChange={event => this.updateDaysValue(event)} required />
+        <input className='days' id='days' type='number' min='0' placeholder='0' onChange={event => this.updateDaysValue(event)} required />
         <span>day(s)</span>
-        <input className='Hours' type='number' min='0'  onChange={event => this.updateHoursValue(event)} required />
+        <input className='hours' id='hours' type='number' min='0' max='24' placeholder='0' onChange={event => this.updateHoursValue(event)} required />
         <span>hours(s)</span>
-        <input className='Minutes' type='number' min='0'  onChange={event => this.updateMinutesValue(event)} required />
+        <input className='minutes' id='minutes' type='number' min='0' max='59' placeholder='0' onChange={event => this.updateMinutesValue(event)} required />
         <span>minutes(s)</span>
+        <input className='seconds' id='seconds' type='number' min='0' max='59' placeholder='0' onChange={event => this.updateSecondsValue(event)} required />
+        <span>seconds(s)</span>
         <button type='submit' onClick={(event) => this.handleSubmit(event, this.state.daysValue, this.state.hoursValue, this.state.minutesValue)}>Set Timer</button>
-        <TimeDisplay time={this.state.selectedTime} />
+        <div>Total Seconds:{this.state.totalSeconds > 0 ? this.state.totalSeconds : 'Countdown Expired'}</div>
+        <div>{timeDisplay}</div>
+        <button type='submit' onClick={() => this.handleStart(this.state.running)}>{startBtn}</button>
+        <button type='submit' onClick={() => this.handleReset()}>Reset</button>
       </div>
     )
   }
