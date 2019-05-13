c048e4df08ffd171447fad5c6dec21f68cf5bb08
Thomas Chun <tchun2055@gmail.com>
2019-05-13 13:58:01 -0700

    updated css

--- src/time-input.js
+++ src/time-input.js
@@ -130,9 +130,11 @@ export default class TimeInput extends React.Component {
         <input className='seconds' id='seconds' type='number' min='0' max='59' placeholder='0' onChange={event => this.updateSecondsValue(event)} required />
         <span> seconds(s)</span>
         <div className='timeDisplay'>{timeDisplay}</div>
+        <div className='btnContainer'>
         <button type='submit' onClick={() => this.handleStart(this.state.running)}>{startBtn}</button>
         <button type='submit' onClick={() => this.handleReset()}>Reset</button>
         <button type='submit' onClick={() => this.handleBid()}>Bid</button>
+        </div>
       </div>
     )
   }
