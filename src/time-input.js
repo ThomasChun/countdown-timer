cf947e25d9833402c6d8dfe0e69b0440456a0461
ThomasChun <tchun2055@gmail.com>
2019-05-13 12:44:37 -0700

    completed basic css

--- src/time-input.js
+++ src/time-input.js
@@ -84,6 +84,14 @@ export default class TimeInput extends React.Component {
     })
   }
 
+  handleBid() {
+    if (this.state.totalSeconds <= 10) {
+      this.setState({
+        totalSeconds: this.state.totalSeconds + 10,
+      })
+    }
+  }
+
   render() {
     let totalSeconds = this.state.totalSeconds;
     let startBtn = '';
@@ -121,10 +129,10 @@ export default class TimeInput extends React.Component {
         <span> minutes(s)</span>
         <input className='seconds' id='seconds' type='number' min='0' max='59' placeholder='0' onChange={event => this.updateSecondsValue(event)} required />
         <span> seconds(s)</span>
-        <div className='totalSecondsDisplay'>Total Seconds: {this.state.totalSeconds > 0 ? this.state.totalSeconds : 'Countdown Expired'}</div>
         <div className='timeDisplay'>{timeDisplay}</div>
         <button type='submit' onClick={() => this.handleStart(this.state.running)}>{startBtn}</button>
         <button type='submit' onClick={() => this.handleReset()}>Reset</button>
+        <button type='submit' onClick={() => this.handleBid()}>Bid</button>
       </div>
     )
   }
