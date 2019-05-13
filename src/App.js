b7b35d13a66052fb1bdb5d3c2b395943308d6de4
Thomas Chun <tchun2055@gmail.com>
2019-05-12 20:07:54 -0700

    added basic styling to page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import InputTime from './time-input';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         Countdown Timer App
       </header>
